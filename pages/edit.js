144dc937b163cf5277367d35d587c6c2cb9b3e6c
Matiss Judins <matissj1337@gmail.com>
2024-09-26 16:57:30 +0300

    started work

---
+++ pages/edit.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EditPage = () => {
+  return (
+    <div>
+      <h1>Edit Page</h1>
+      <p>Welcome to the edit page!</p>
+    </div>
+  );
+};
+
+export default EditPage;
